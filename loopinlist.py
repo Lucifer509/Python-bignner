83be6f2f0516f9481b6ca074a91bf68f73c94231
Lucifer <jhagaurav2019@gmail.com>
2021-05-21 14:42:30 +0530

    done bro

--- loopinlist.py
+++
@@ -1,8 +0,0 @@
-list1 = [int, float, 4, "Champ", "Package", 99, 56, 0, "Prize", 6]
-for item in list1:
-
-    if str(item).isnumeric() and item >= 6:  # here we use str(item) which convert every value in list1 in str and
-        # .isnumeric check if the store value in string is numeric.
-        print(item)
-
-
